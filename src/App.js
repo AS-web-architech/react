f7fd44ea7196fbc864f0d633be74d5883b08ecf9
Areeba Shafi <areebashafiaptechn@gmail.com>
2023-02-09 01:22:50 +0500

    practice

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+import Card from './components/card';
+const App= ()=> {
+  return (
+ <>
+  <Navbar/>
+ </>
+ 
+  )
+}
+
+export default App;
